1c6b6d87b9d3d3bdace825e5cf992ee9f19e98e8
kapil-malviya <122611483+kapil-malviya@users.noreply.github.com>
2023-09-19 23:43:19 +0530

    Time: 47 ms (55.60%), Space: 16.2 MB (72.17%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        
+        num11 = int(num1)
+        num22 = int(num2)
+        
+        result = num11*num22
+        
+        return str(result)
