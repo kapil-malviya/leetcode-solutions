d42678e128bb1cbf6c850b07f5b2786361153c0a
kapil-malviya <122611483+kapil-malviya@users.noreply.github.com>
2023-09-20 00:03:50 +0530

    Time: 137 ms (18.12%), Space: 16.4 MB (39.51%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,9 +1,32 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        
+        '''        
         num11 = int(num1)
         num22 = int(num2)
         
         result = num11*num22
         
         return str(result)
+        '''
+
+        result = [0] * (len(num1) + len(num2))
+    
+        # converting string to integer in reverse order
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+    
+        # multiplication digit by digit
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                result[i + j] += int(num1[i]) * int(num2[j])
+                result[i + j + 1] += result[i + j] // 10
+                result[i + j] %= 10
+    
+        # removing leading zeros from result
+        while len(result) > 1 and result[-1] == 0:
+            result.pop()
+    
+        # convert result list to string and reverse it
+        result = ''.join(map(str, result[::-1]))
+    
+        return result
