838dd1e02d46e4956cd0b13e03b9b6bf1d214cac
kapil-malviya <122611483+kapil-malviya@users.noreply.github.com>
2023-10-23 14:20:26 +0530

    Time: 31 ms (95.10%), Space: 16.2 MB (70.44%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,6 +1,12 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+        if n<=0:
+            return False
         
+        log_value = math.log(n)/math.log(4)
+        return int(log_value) == log_value
+        
+    ''' 
         if n<=0:
             return False
         
@@ -10,3 +16,8 @@ class Solution:
             n //= 4
             
         return True
+    
+    '''
+        
+
+    
